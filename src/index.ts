73607723af038737dfdf471200c125b50ffa778a
woywro <woywro@gmail.com>
2023-09-21 11:32:07 +0200

    refactor: improve error handling

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,11 @@ import { NextMiddlewareResult } from 'next/dist/server/web/types'
 import { z } from 'zod'
 import { LRUCache } from 'typescript-lru-cache'
 
-const MAINTENANCE_KEY_MISSING = 'Maintenance mode key is not set'
+const DEFAULT_MAINTENANCE_PAGE_SLUG = '/maintenance'
+const DEFAULT_MAINTENANCE_MODE_KEY = 'isInMaintenanceMode'
+const DEFAULT_CACHE_TIME = 60000
+
+const MAINTENANCE_KEY_MISSING = 'Maintenance mode key is not found in the specified provider'
 
 type Provider = 'upstash' | 'edge-config'
 
@@ -41,8 +45,9 @@ const MiddlewareConfig = z
 
 const MaintenanceModeOptions = z.object({
   provider: z.enum(['upstash', 'edge-config']),
-  maintenancePageSlug: z.string().optional(),
-  key: z.string().optional(),
+  maintenancePageSlug: z.string().optional().nullable(),
+  key: z.string().optional().nullable(),
+  cacheTime: z.number().optional().nullable(),
 })
 
 const MaintenanceModeConfig = z
@@ -69,7 +74,7 @@ const handleMaintenanceMode = async (
   options: MiddlewareFactoryOptions,
   req: NextRequest,
 ): Promise<NextMiddlewareResult> => {
-  const maintenancePageSlug = options?.maintenancePageSlug || '/maintenance'
+  const maintenancePageSlug = options?.maintenancePageSlug ?? DEFAULT_MAINTENANCE_PAGE_SLUG
   if (isInMaintenanceMode) {
     req.nextUrl.pathname = maintenancePageSlug
     return NextResponse.rewrite(req.nextUrl)
@@ -83,7 +88,7 @@ const getIsInMaintenanceMode = async (
   cache?: LRUCache<string>,
 ): Promise<boolean | null | undefined> => {
   const provider = options?.provider
-  const key = options?.key
+  const key = options?.key ?? DEFAULT_MAINTENANCE_MODE_KEY
   const cacheKey = `${provider}-${connectionString}-${key}`
 
   if (!!cache && cache.has(cacheKey)) {
@@ -98,13 +103,13 @@ const getIsInMaintenanceMode = async (
         const [url, token] = connectionString.split('@')
         const redis = new Redis({ url: url, token: token })
 
-        result = await redis.get<boolean | null>(key || 'isInMaintenanceMode')
+        result = await redis.get<boolean | null>(key)
         break
       }
       case 'edge-config': {
         const edgeConfig = createClient(connectionString)
 
-        result = await edgeConfig.get<boolean | undefined>(key || 'isInMaintenanceMode')
+        result = await edgeConfig.get<boolean | undefined>(key)
         break
       }
       default:
@@ -142,14 +147,14 @@ const providerMiddleware = async ({ req, _next, middleware, connectionString, op
       if (afterCheckResult) return afterCheckResult
     }
 
-    return maintenanceResult ?? NextResponse.next()
+    return maintenanceResult
   } catch (e) {
     if (e instanceof Error) throw new Error(e.message)
     else throw new Error('Unknown error')
   }
 }
 
-export const withMaintenanceMode = (
+const withMaintenanceMode = (
   { beforeCheck, afterCheck }: { beforeCheck?: NextMiddleware; afterCheck?: NextMiddleware },
   connectionString: string,
   options: MiddlewareFactoryOptions,
@@ -158,7 +163,12 @@ export const withMaintenanceMode = (
     throw new Error('At least one function (beforeCheck or afterCheck) should be passed')
   }
   const cacheTime = options?.cacheTime
-  const cache = !!cacheTime ? new LRUCache({ maxSize: 1, entryExpirationTimeInMS: cacheTime }) : undefined
+  const cache = !!cacheTime
+    ? new LRUCache({
+        maxSize: 1,
+        entryExpirationTimeInMS: cacheTime ?? DEFAULT_CACHE_TIME,
+      })
+    : undefined
   return async (req: NextRequest, _next: NextFetchEvent): Promise<NextMiddlewareResult> => {
     const parseResult = MaintenanceModeConfig.safeParse({
       middleware: { beforeCheck, afterCheck },
@@ -177,4 +187,80 @@ export const withMaintenanceMode = (
   }
 }
 
-export default withMaintenanceMode
+const ToggleOptionsSchema = z
+  .object({
+    provider: z.enum(['upstash', 'edge-config']),
+    connectionString: z.string(),
+    key: z.string().optional(),
+    maintenanceEdgeConfigId: z.string().optional(),
+    maintenanceModeVercelApiToken: z.string().optional(),
+  })
+  .refine(
+    (data) => {
+      return (
+        (data.provider === 'upstash' && data.connectionString.includes('upstash')) ||
+        (data.provider === 'edge-config' && data.connectionString.includes('edge-config'))
+      )
+    },
+    {
+      message: 'Invalid connection string for the selected provider',
+      path: ['connectionString'],
+    },
+  )
+  .refine(
+    (data) => {
+      return data.provider === 'edge-config' && !!data.maintenanceEdgeConfigId && !!data.maintenanceModeVercelApiToken
+    },
+    {
+      message: 'Missing maintenanceEdgeConfigId or maintenanceModeVercelApiToken',
+      path: ['maintenanceEdgeConfigId', 'maintenanceModeVercelApiToken'],
+    },
+  )
+
+type ToggleOptions = z.infer<typeof ToggleOptionsSchema>
+
+const updateMaintenanceModeStatus = async (isActive: boolean, options: ToggleOptions) => {
+  try {
+    const parseResult = ToggleOptionsSchema.safeParse(options)
+    if (!parseResult.success) throw new Error(parseResult.error.message)
+    const { provider, connectionString, key, maintenanceEdgeConfigId, maintenanceModeVercelApiToken } = parseResult.data
+
+    switch (provider) {
+      case 'upstash': {
+        const [url, token] = connectionString.split('@')
+        const redis = new Redis({ url: url, token: token })
+        await redis.set<boolean | null>(key ?? DEFAULT_MAINTENANCE_MODE_KEY, isActive)
+        break
+      }
+      case 'edge-config': {
+        await fetch(`https://api.vercel.com/v1/edge-config/${maintenanceEdgeConfigId}/items`, {
+          method: 'PATCH',
+          headers: {
+            Authorization: `Bearer ${maintenanceModeVercelApiToken}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            items: [
+              {
+                operation: 'update',
+                key: key ?? DEFAULT_MAINTENANCE_MODE_KEY,
+                value: isActive,
+              },
+            ],
+          }),
+        })
+        break
+      }
+      default:
+        throw new Error(`Unsupported provider: ${provider}`)
+    }
+  } catch (e) {
+    if (e instanceof Error) {
+      throw new Error(e.message)
+    } else {
+      throw new Error('Unknown error')
+    }
+  }
+}
+
+export { withMaintenanceMode, updateMaintenanceModeStatus }
